cb06e5f56a1a632801c6077a5eae60cd3afefa6c
phillip <phillip.iwanaga@gmail.com>
2022-02-10 18:20:12 -0800

    added number generator

--- src/Hero.js
+++ src/Hero.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import HeroImg from './images/coinbase-hero.jpg';
 
 const Hero = () => {
+    const [ numPeople, setNumPeople ] = useState(10);
+
+    const generateNumber = () => {
+        return Math.floor(Math.random() * 100);
+    };
+
     const handleSubmit = (evt) => {
         evt.preventDefault();
+        setNumPeople(generateNumber());
     };
 
     return (
@@ -16,12 +23,12 @@ const Hero = () => {
                 <div className='Hero-right'>
                     <div className='Hero-right-text'>
                         <h1 className='Hero-right-heading'>The future of money is here.</h1>
-                        <h2 className='Hero-right-subheading'>Over 73 million people and businesses trust us to buy, sell, and manage crypto</h2>
+                        <h2 className='Hero-right-subheading'>Over {numPeople} million people and businesses trust us to buy, sell, and manage crypto</h2>
                     </div>
                     <div className='Hero-right-signup'>
                         <form className='Hero-signup-form' onSubmit={handleSubmit}>
                             <input type='text' placeholder='person@email.com' className='Hero-signup-input'/>
-                            <button className='Hero-signup-button'>Sign up</button>
+                            <button className='Hero-signup-button'>Click Me</button>
                         </form>
                         <h4 className='Hero-signup-offer'>Get $5 in Bitcoin for signing up*</h4>
                     </div>
